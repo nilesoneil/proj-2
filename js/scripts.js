b54b0e226dc81a7857f2031d352316079dbfaa98
Niles O'Neil <nilesoneil@gmail.com>
2022-08-28 07:33:39 -0700

    wip scripts

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+(document).ready(function(event) {
+  ("form#survey").submit(function(event) {
+
+    const fruits = ("#fruits").val();
+    const cuisine = ("#cuisine").val();
+    const pets = ("#pets").val();
+    const gender = ("#gender").val();
+    const season =("#season").val();
+
+    
+    if (fruits === "kiwi" || cuisine === "thai" && gender === "female") {
+      ("#ruby").show();
+      ("#javascript").hide();
+      ("#rust").hide();
+    } else if (fruits === "banana" || cuisine === "italian" || gender === "male") {
+      ("#ruby").hide();
+      ("#javascript").show();
+      ("#rust").hide();
+    } else if (fruits === "peach" || cuisine === "mexican" || season === "winter") {
+      ("#ruby").hide();
+      ("#javascript").hide();
+      ("#rust").show();
+    } else {
+      ("#ruby").hide();
+      ("#javascript").hide();
+      ("#rust").show();
+    }
+    event.preventDefault();
+  });
+});
