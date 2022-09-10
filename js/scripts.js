80dc1a10d712927c334a766ffd4e8dc617a15a83
Niles O'Neil <nilesoneil@gmail.com>
2022-09-10 12:46:02 -0700

    trying to get the messages to show

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,33 @@
-(document).ready(function(event) {
-  ("form#survey").submit(function(event) {
-
-    const fruits = ("#fruits").val(
-    const cuisine = ("#cuisine").val();
-    const pets = ("#pets").val();
-    const gender = ("#gender").val();
-    const season =("#season").val();
+function addFormListener() {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", onFormSubmit)
+}
+function onFormSubmit(event) {
+  event.preventDefault();
+    const fruits = document.querySelector("#fruits").value
+    const dino = document.querySelector("#dino").value
+    const pets = document.querySelector("#pets").value
+    const gender = document.querySelector("#gender").value
+    const season = document.querySelector("#season").value
 
     
-    if (fruits === "kiwi" || cuisine === "thai" && gender === "female") {
-      ("#ruby").show();
-      ("#javascript").hide();
-      ("#rust").hide();
-    } else if (fruits === "banana" || cuisine === "italian" || gender === "male") {
-      ("#ruby").hide();
-      ("#javascript").show();
-      ("#rust").hide();
-    } else if (fruits === "peach" || cuisine === "mexican" || season === "winter") {
-      ("#ruby").hide();
-      ("#javascript").hide();
-      ("#rust").show();
+    if (fruits === "kiwi" || (dino === "tRex" && gender === "female")) {
+      document.querySelector("#ruby").show();
+      document.querySelector("#javascript").hide();
+      document.querySelector("#rust").hide();
+    } else if (fruits === "banana" || dino === "stego" || gender === "male") {
+      document.querySelector("#ruby").hide();
+      document.querySelector("#javascript").show();
+      document.querySelector("#rust").hide();
+    } else if (fruits === "peach" || dino === "tTops" || season === "winter") {
+      document.querySelector("#ruby").hide();
+      document.querySelector("#javascript").hide();
+      document.querySelector("#rust").show();
     } else {
-      ("#ruby").hide();
-      ("#javascript").hide();
-      ("#rust").show();
+      document.querySelector("#ruby").hide();
+      document.querySelector("#javascript").hide();
+      document.querySelector("#rust").show();
     }
-    event.preventDefault();
-  });
-});
+   
+  };
+window.addEventListener("load", addFormListener);
